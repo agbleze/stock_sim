e1317f4d53c9a08e722a863c9b0101abe57c4253
linus <lagbleze@st.ug.edu.gh>
2025-03-18 23:46:41 +0100

    add test file

---
+++ test_flytekit.py
@@ -0,0 +1,14 @@
+
+from flytekit import task, workflow
+
+@task
+def hello() -> str:
+    return "Hello, World"
+
+@workflow
+def hello_wf():
+    res = hello()
+    return res
+
+if __name__ == "__main__":
+    print(f"running flyte code {hello_wf()}")
