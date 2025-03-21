6890ebc4324ef98c7afde5bba25446f124fabd44
linus <lagbleze@st.ug.edu.gh>
2025-03-21 23:38:15 +0100

    update

--- analysis.py
+++ analysis.py
@@ -272,17 +272,6 @@ numpy_array = tensor.numpy()
 
 # Display the NumPy array
 print(numpy_array)
-
-# %%
-import numpy as np
-d = 64                           # dimension
-nb = 100000                      # database size
-nq = 10000                       # nb of queries
-np.random.seed(1234)             # make reproducible
-xb = np.random.random((nb, d)).astype('float32')
-xb[:, 0] += np.arange(nb) / 1000.
-xq = np.random.random((nq, d)).astype('float32')
-xq[:, 0] += np.arange(nq) / 1000.
 # %%
 import faiss                   # make faiss available
 index = faiss.IndexFlatL2(d)   # build the index
